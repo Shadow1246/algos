817bdff3074c4c9b3a414c20417045a066fb9497
Shadow1246 <Shadow1246@users.noreply.github.com>
2018-03-21 13:29:27 -0400

    Added sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -12,24 +12,40 @@ using namespace std;
 \param [in] data The data set that will be searched
 \returns location of key if found or -1 if not found
 */
-vector<string> sort(auto data)
-{
- for(auto i = 0u; i < data.size(); i++)
-   {    //Hint: when Data is changed to a vector of objects, we must call
-        //the method to get a character from the object Data[i]
+int linearSearch(auto data, auto key){
+for(auto i = 0u; i < data.size(); i++) 
+   {    
+     if (data[i] == key)
+      return i;
+  }
+   return -1;//not found
 
-	int small =i;
-	 for(auto j= i + 1; j < data.size(); j++)
+}
+
+void insertionSort(auto & D){
+	int passes = 0;
+	int p=0;
+	do{
+	    if (passes %20000 == 0)
+	    cout<< " passes completed: "<< passes <<endl;
+
+	for ( unsigned int i=1; i< D.size();i++)
+	
 	{
-     if (data[j]<data[small])
-	small=j;
-	}
-		swap(data[i],data[small]);
-	}
-   //   return i;
- // }
-  // return -1;//not found
-	return data;
+	  int j = i ;
+	    while (j>0 and D[j]<D[j-1])
+	      { 
+
+		 swap (D[j], D[j-1]);
+		  j = j-1;
+
+	     }
+		passes = passes + 1;
+	    }
+		p++;
+	}while (p > 0);
+	cout<< " Final passes completed: "<< passes <<endl;
+	
 
 }
 
@@ -37,7 +53,7 @@ int main()
 {
   vector<string> inputs;
   string search_key, input;
-  vector<string> result;
+ 
 
    cout<<"Welcome to \"search it\". We first need some input data."<<endl;
    cout<<"We'll assume the inputs do not have any spaces."<<endl<<endl;
@@ -50,7 +66,7 @@ int main()
        inputs.push_back(input);
        cin>>input;
     }
-
+	
    cout<<endl<<"["<<inputs.size()<<" values read from input source]"<<endl<<endl;
 
   if(inputs.size() == 0)//no input
@@ -59,17 +75,14 @@ int main()
        exit(1);//nothing to do but quit program
   }
  
-   cout<<endl<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
- 
-  result = sort(inputs);
-
-	
-	for(unsigned int i=0;i<result.size();i++)
+   insertionSort(inputs);
+    cout << "data to be sorted"<<endl;
+    for ( unsigned int s= 0; s <inputs.size();s++)
 	{
-		cout<<result[i]<<endl;
+    	   cout << inputs[s]<<endl;
 	}
-
    cout<<endl<<"Program \"search it\" is now finished."<<endl<<endl;
 
     return 0;
+
 }
